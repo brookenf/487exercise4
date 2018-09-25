2aa41982a0220691b02fc9d4ece851563bad35fb
Brooke Fisher <brookenfish1@gmail.com>
2018-09-25 07:58:01 -0400

    Added the files for my hamburger button and tooltip demo

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+//JS page for js_demo
+console.log('DOM is loaded');
+
+//Get your objects
+var burger = document.getElementById('burger');
+var mobileNav = document.getElementById('offscreen-nav');
+var closeBtn = document.getElementById('close-btn');
+var content = document.getElementById('content');
+var moreInfoText = document.getElementById('more-info');
+var tooltip = document.getElementById('tooltip');
+
+//Adding the click events
+burger.addEventListener('click', function(){
+  console.log('Button clicked');
+  mobileNav.style.width = '40%';
+  mobileNav.style.transition = '0.5 ease';
+  content.style.opacity = '0.1';
+});
+
+closeBtn.addEventListener('click', function(){
+  console.log('Close the menu');
+  mobileNav.style.width = '0%';
+  mobileNav.style.transition = '0.5s ease';
+  content.style.opacity = '1';
+});
+
+moreInfoText.addEventListener('mouseenter',function(event){
+  //code goes here
+  tooltip.style.opacity = '1';
+  var x = event.pageX;     // Get the horizontal coordinate
+  var y = event.pageY;     // Get the vertical
+  console.log(y);
+  tooltip.style.top = y + 'px';
+  tooltip.style.right = x + 'px';
+  tooltip.style.transition = '0.2 ease';
+});
+
+moreInfoText.addEventListener('mouseout', function(){
+  console.log('Leaving the mouse');
+  tooltip.style.opacity = '0';
+});
